988eaf564923f400bb3768a95b382b1a04a846c5
Gaston  Mira <gastonmira@gmail.com>
2017-01-31 23:53:03 -0300

    Initial commit

---
+++ app/src/main/java/com/gastonmira/flickrsearch/UrlManager.java
@@ -0,0 +1,53 @@
+package com.gastonmira.flickrsearch;
+
+import android.net.Uri;
+
+/**
+ * Created by gastonmira on 31/1/17.
+ */
+
+public class UrlManager {
+    private static final String API_KEY = "3861b5348a35dc8ee6e66346f14f2712";
+    private static final String ENDPOINT = "https://api.flickr.com/services/rest/";
+    private static final String METHOD_GETRECENT = "flickr.photos.getRecent";
+    private static final String METHOD_SEARCH = "flickr.photos.search";
+
+    private static volatile UrlManager instance = null;
+    private UrlManager() {
+
+    }
+
+    public static UrlManager getInstance() {
+        if (instance == null) {
+            synchronized (UrlManager.class) {
+                if (instance == null) {
+                    instance = new UrlManager();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public static String getItemUrl(String query, int page) {
+        String url;
+        if (query != null) {
+            url = Uri.parse(ENDPOINT).buildUpon()
+                    .appendQueryParameter("method", METHOD_SEARCH)
+                    .appendQueryParameter("api_key", API_KEY)
+                    .appendQueryParameter("format", "json")
+                    .appendQueryParameter("nojsoncallback", "1")
+                    .appendQueryParameter("text", query)
+                    .appendQueryParameter("page", String.valueOf(page))
+                    .build().toString();
+        } else {
+            url = Uri.parse(ENDPOINT).buildUpon()
+                    .appendQueryParameter("method", METHOD_GETRECENT)
+                    .appendQueryParameter("api_key", API_KEY)
+                    .appendQueryParameter("format", "json")
+                    .appendQueryParameter("nojsoncallback", "1")
+                    .appendQueryParameter("page", String.valueOf(page))
+                    .build().toString();
+        }
+        return url;
+    }
+}
